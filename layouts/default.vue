d4dde7ccdc9c5a8f2dc16351addd20a8da14d785
gnpashi <guynatan@gmail.com>
2023-03-12 16:04:51 +0200

    font

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,27 +8,44 @@
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-    head() {
-      return {
-        title: "יאמי!",
-        htmlAttrs: {
+  head() {
+  return {
+    title: "יאמי!",
+    htmlAttrs: {
       lang: 'he',
       dir: 'rtl'
     },
-        link: [
-          {
-            rel: 'stylesheet',
-            type:"text/css",
-            href: 'https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css'
-          }
-        ]
-       }
+    link: [
+      {
+        rel: 'stylesheet',
+        type:"text/css",
+        href: 'https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css'
+      },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.googleapis.com'
+      },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+        crossorigin: true
+      },
+      {
+        href: 'https://fonts.googleapis.com/css2?family=Varela+Round&display=swap"',
+        rel: 'stylesheet'
+      }
+    ]
   }
+}
+
 })
 </script>
 
 <style scoped>
   html{
     scroll-behavior: smooth;
+  }
+  *{
+    font-family: 'Varela Round', sans-serif !important;
   }
 </style>
