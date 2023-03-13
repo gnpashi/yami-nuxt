e09518e4df8abc5a9c235d6d9389554d01f56581
gnpashi <guynatan@gmail.com>
2023-03-13 19:52:12 +0200

    filters

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,30 +1,38 @@
 
 
 <template>
-    <nav class="navbar has-background-link-dark px-3" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-        <h1 class="title is-flex is-align-items-center is-justify-content-center">
-            <NuxtLink to="/" class="has-text-white">יאמי!</NuxtLink>
-        </h1>
-        <a  @click="isOpen = !isOpen" role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        </a>
-    </div>
-
-        <div :class="{ 'open': isOpen }" id="navbar" class="navbar-menu has-background-link-dark">
-            <div class="navbar-start">
-            
-            </div>
-            
-            <div class="navbar-end ">
-                <NuxtLink to="/#clients" class="navbar-item">לקוחות מספרים</NuxtLink>
-                <NuxtLink to="/#companies" class="navbar-item">חברות</NuxtLink>
-                <NuxtLink to="/products" class="navbar-item">למוצרים</NuxtLink>
-            </div>
+    <div>
+        <div class="notification is-warning py-1">
+           <div class=" is-flex is-justify-content-center is-align-items-center ">
+               לרכישה <a class="mx-2" href="tel:0525809898">התקשרו 0525809898</a>
+           </div>
         </div>
-    </nav>
+        <nav class="navbar has-background-link-dark px-3" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <h1 class="title is-flex is-align-items-center is-justify-content-center">
+                <NuxtLink to="/" class="has-text-white">יאמי!</NuxtLink>
+            </h1>
+            <a  @click="isOpen = !isOpen" role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            </a>
+        </div>
+    
+            <div :class="{ 'open': isOpen }" id="navbar" class="navbar-menu has-background-link-dark">
+                <div class="navbar-start">
+                
+                </div>
+                
+                <div class="navbar-end ">
+                    <NuxtLink to="/#clients" class="navbar-item">לקוחות מספרים</NuxtLink>
+                    <NuxtLink to="/#companies" class="navbar-item">חברות</NuxtLink>
+                    <NuxtLink to="/products" class="navbar-item">למוצרים</NuxtLink>
+                </div>
+            </div>
+        </nav>
+
+    </div>
 </template>
 
 <script>
@@ -44,7 +52,7 @@
     }
 </script>
 
-<style> 
+<style scoped> 
     .navbar-burger{
         margin-right: auto;
         margin-left: 0;
@@ -64,6 +72,9 @@
     }
     .navbar-end{
         margin-left: 0;
+    }
+    .notification{
+        margin-bottom: 0;
     }
     @media screen and (min-width: 768px) {
         .navbar{
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -11,6 +11,33 @@
       </div>
       <label for="search">חיפוש</label>
       <input class="input mb-3" type="text" @input="search" name="search">
+<!-- 
+      <div>
+        <label for="brandFilter">מותג</label>
+        <select class="select" id="brandFilter" ref="brandFilter" @change="filterProducts">
+          <option value="all">All</option>
+          <option  v-for="brand in brands" :key="brand" :value="brand">{{brand}}</option>
+
+        </select>
+      </div>
+      <div>
+        <label for="ageFilter">גיל</label>
+        <select class="select" id="ageFilter" ref="ageFilter" @change="filterProducts">
+          <option value="all">All</option>
+          <option  v-for="age in ages" :key="age" :value="age">{{age}}</option>
+        </select>
+      </div> -->
+  <div class="is-flex ">
+    <div class="is-flex is-flex-direction-column	" v-for="filter in filters" :key="filter.id">
+      <label :for="filter.id">{{ filter.label }}</label>
+      <div class="select">
+        <select class="select" :id="filter.id" :ref="filter.id" v-model="filter.value" @change="filterProducts">
+          <option value="all">הכל</option>
+          <option v-for="value in filter.values" :key="value" :value="value">{{ value }}</option>
+        </select>
+      </div>
+    </div>
+  </div>
       <div class="columns is-multiline">
         <div class="column is-4" v-for="product in filteredProducts" :key="product.id">
           <Product :products="products" :product="product" :currentUser="currentUser" @productDeleted="fetchProducts"/>
@@ -19,6 +46,8 @@
     </div>
   </div>
 </template>
+
+
 
 <script>
 export default {
@@ -30,7 +59,25 @@ export default {
         email: 'אין משתמש פעיל'
       },
       products: [],
-      filteredProducts: [] // Define filteredProducts property
+      filteredProducts: [],
+      filterBrand: 'all',
+      filterAge: 'all',
+      filters: {
+        brands:{
+          id: 'brandFilter',
+          label: 'מותג',
+          values: [],
+          value: 'all'
+        },
+        ages:{
+          id: 'ageFilter',
+          label: 'גיל',
+          values: [],
+          value: 'all'
+        },
+      },
+      brandFilter: 'all',
+      ageFilter: 'all',
     }
   },
   created() {
@@ -43,22 +90,51 @@ export default {
         const products = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
         this.products = products
         this.filteredProducts = products // Update filteredProducts when products change
+        this.filters.brands.values = [...new Set(products.map(product => product.brand))]
+        this.filters.ages.values = [...new Set(products.map(product => product.age))]
       } catch (error) {
         console.error('Error fetching products:', error)
       }
     },
     search(event){
   const searchTerm = event.target.value.toLowerCase()
-  this.filteredProducts = this.products.filter(product => 
-    (product.name && product.name.toLowerCase().includes(searchTerm)) ||
-    (product.description && product.description.toLowerCase().includes(searchTerm)) ||
-    (product.age && product.age.toLowerCase().includes(searchTerm)) ||
-    (product.price && product.price.toLowerCase().includes(searchTerm)) ||
-    (product.brand && product.brand.toLowerCase().includes(searchTerm)) ||
-    (product.allergies && product.allergies.toLowerCase().includes(searchTerm))
-  )
-}
+  // const brandFilter = this.$refs.brandFilter.value
+  // const ageFilter = this.$refs.ageFilter.value
+      console.log(this.filterBrand);
+  this.filteredProducts = this.products.filter(product => {
+      const matchesSearchTerm = (
+      (product.name && product.name.toLowerCase().includes(searchTerm)) ||
+      (product.description && product.description.toLowerCase().includes(searchTerm)) ||
+      (product.price && product.price.toLowerCase().includes(searchTerm)) ||
+      (product.allergies && product.allergies.toLowerCase().includes(searchTerm)) ||
+      (product.brand && product.brand.toLowerCase().includes(searchTerm)) ||
+      (product.age && product.age.toLowerCase().includes(searchTerm))
+      )
+      return matchesSearchTerm
+    // const matchesBrandFilter = (brandFilter === 'all' || product.brand === brandFilter)
+  //  const matchesAgeFilter = (ageFilter === 'all' || product.age === ageFilter)
 
+    // return matchesSearchTerm && matchesBrandFilter && matchesAgeFilter
+  })
+  },
+  // filterProducts(event){
+  //   const brandFilter = event.target.value
+  //   this.filteredProducts = this.products.filter(product => {
+  //     const matchesBrandFilter = (brandFilter === 'all' || product.brand === brandFilter)
+  //     const matchesBrandFilter = (brandFilter === 'all' || product.brand === brandFilter)
+  //     return matchesBrandFilter
+  //   })
+
+  // }
+  filterProducts(event) {
+    this.filteredProducts = this.products.filter(product => {
+      const matchesBrandFilter = ( this.filters.brands.value === 'all' || product.brand ===  this.filters.brands.value)
+      const matchesAgeFilter = ( this.filters.ages.value === 'all' || product.age ===  this.filters.ages.value)
+
+      return matchesBrandFilter && matchesAgeFilter
+    })
+    console.log(this.brandFilter);
+  }
   },
   mounted() {
     this.$fire.auth.onAuthStateChanged(user => {
@@ -80,3 +156,10 @@ export default {
   }
 }
 </script>
+
+
+<style>
+  .is-flex{
+    column-gap: 1rem
+  }
+</style>
